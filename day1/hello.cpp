203516652c5bb0227022d5c68d2c0553e777cb45
sam kim <seungkim11@gmail.com>
2016-07-22 14:52:32 -0700

    initiating branch

---
+++ day1/hello.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!\n";
+
+  // // integers
+  // int age = 13;
+  //
+  // std::cout << age;
+
+  int age1 = 14.5;
+  float age2 = 14.5;
+  char c = 'A';
+  string name = "Matt";
+
+  cout << age1 << endl;
+  cout << age2 << endl;
+  cout << c << endl;
+  cout << name << endl;
+
+  float a = 5;
+  float b = 7;
+  
+
+  cout << a / b << endl;
+}
